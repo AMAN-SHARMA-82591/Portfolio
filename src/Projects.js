cff39a5916eee26aa2835fc1de733c127ff58fcd
Aman <aman82591@gmail.com>
2025-04-08 12:12:21 +0530

    Added new entity in projects section.

--- src/Projects.js
+++ src/Projects.js
@@ -1,5 +1,6 @@
 import React from "react";
 import TMSImage from "./Images/TMS.jpeg";
+import cyioImage from "./Images/cyio.png";
 import { motion } from "framer-motion";
 import { fadeIn } from "./variants";
 
@@ -7,6 +8,44 @@ function Projects() {
   return (
     <div className="projects-container">
       <h1 className="pr-cont-heading">Projects</h1>
+      <motion.div
+        variants={fadeIn("left", 0.2)}
+        initial={{ opacity: 0, x: 100 }}
+        whileInView={"show"}
+        viewport={{ once: true, amount: 0.2 }}
+        className="pr-cont-sec-1"
+      >
+        <div className="pr-sec-item-1">
+          <div className="pr-sec-item-1-img">
+            <img alt="prImage" src={cyioImage}></img>
+            <div className="item-img-hover">
+              <a href="https://darklight.ai/" target="_blank" rel="noreferrer">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 512 512"
+                  height="2rem"
+                  width="2rem"
+                  fill="black"
+                >
+                  <path d="M352 256c0 22.2-1.2 43.6-3.3 64l-185.3 0c-2.2-20.4-3.3-41.8-3.3-64s1.2-43.6 3.3-64l185.3 0c2.2 20.4 3.3 41.8 3.3 64zm28.8-64l123.1 0c5.3 20.5 8.1 41.9 8.1 64s-2.8 43.5-8.1 64l-123.1 0c2.1-20.6 3.2-42 3.2-64s-1.1-43.4-3.2-64zm112.6-32l-116.7 0c-10-63.9-29.8-117.4-55.3-151.6c78.3 20.7 142 77.5 171.9 151.6zm-149.1 0l-176.6 0c6.1-36.4 15.5-68.6 27-94.7c10.5-23.6 22.2-40.7 33.5-51.5C239.4 3.2 248.7 0 256 0s16.6 3.2 27.8 13.8c11.3 10.8 23 27.9 33.5 51.5c11.6 26 20.9 58.2 27 94.7zm-209 0L18.6 160C48.6 85.9 112.2 29.1 190.6 8.4C165.1 42.6 145.3 96.1 135.3 160zM8.1 192l123.1 0c-2.1 20.6-3.2 42-3.2 64s1.1 43.4 3.2 64L8.1 320C2.8 299.5 0 278.1 0 256s2.8-43.5 8.1-64zM194.7 446.6c-11.6-26-20.9-58.2-27-94.6l176.6 0c-6.1 36.4-15.5 68.6-27 94.6c-10.5 23.6-22.2 40.7-33.5 51.5C272.6 508.8 263.3 512 256 512s-16.6-3.2-27.8-13.8c-11.3-10.8-23-27.9-33.5-51.5zM135.3 352c10 63.9 29.8 117.4 55.3 151.6C112.2 482.9 48.6 426.1 18.6 352l116.7 0zm358.1 0c-30 74.1-93.6 130.9-171.9 151.6c25.5-34.2 45.2-87.7 55.3-151.6l116.7 0z" />
+                </svg>
+              </a>
+            </div>
+          </div>
+          <div className="pr-sec-item-1-text">
+            <h1>Cyio - Cyber Security Application</h1>
+            <p>
+              Developed a cyber security application, utilizing{" "}
+              <span>React</span> for the front-end and <span>Relay-Client</span>{" "}
+              for seamless integration of <span>GraphQL</span> queries and
+              mutations. Implemented a dynamic user-friendly interface, ensuring
+              secure and efficient task creation, updates, and management.
+              <span>Optimized</span> GraphQL communication, enhancing data fetching
+              performance and scalability within the application.
+            </p>
+          </div>
+        </div>
+      </motion.div>
       <motion.div
         variants={fadeIn("left", 0.2)}
         initial={{ opacity: 0, x: 100 }}
